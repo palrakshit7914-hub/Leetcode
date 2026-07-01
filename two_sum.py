5c2d29a2f4bc3baca4da3a3f5dfbb551332787ad
Rakshit <palrakshit7914@gmail.com>
2026-07-01 23:49:58 +0530

    first commit

---
+++ two_sum.py
@@ -0,0 +1,13 @@
+# ----For the leetcode problem "Two Sum"----
+
+class Solution(object):
+    def two_sum(self, nums, target):
+        
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        return []
+
