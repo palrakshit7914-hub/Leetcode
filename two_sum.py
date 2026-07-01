67e424f6a4493ec385830761373d17277a2fc2d4
Rakshit <palrakshit7914@gmail.com>
2026-07-01 23:58:06 +0530

    Refactor two_sum implementation and add example usage

--- two_sum.py
+++ two_sum.py
@@ -10,4 +10,20 @@ class Solution(object):
                 return [num_map[complement], i]
             num_map[num] = i
         return []
+# --- For your own value-----
 
+class Solution:
+    def two_sum(self, nums, target):
+        num_map = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_map:
+                return [num_map[complement], i]
+            num_map[num] = i
+        return []
+    
+nums = [2, 7, 11, 15]
+target = 9
+solution = Solution()
+result = solution.two_sum(nums, target)
+print(result)  
