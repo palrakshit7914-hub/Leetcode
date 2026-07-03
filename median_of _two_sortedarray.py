d46287cc92cc4a28ee9fab58356a304e77d8e37c
Rakshit <palrakshit7914@gmail.com>
2026-07-04 01:08:04 +0530

    Add example usage for findMedianSortedArrays function

--- median_of _two_sortedarray.py
+++ median_of _two_sortedarray.py
@@ -8,4 +8,7 @@ class Solution(object):
             mid1 = merged[n // 2 - 1]
             mid2 = merged[n // 2]
             return (mid1 + mid2) / 2.0
-        
+nums1 = [1, 3]
+nums2 = [2, 4]
+solution = Solution()
+print(solution.findMedianSortedArrays(nums1, nums2))
